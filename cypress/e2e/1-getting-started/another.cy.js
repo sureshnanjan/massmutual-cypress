9ae93bba09af1ae5882650f7939a9b01c3b0ca32
sureshnanjan <sureshnanjan@outlook.com>
2024-01-30 14:32:59 +0530

    Simple Activity for NodeJS Hands on

---
+++ cypress/e2e/1-getting-started/another.cy.js
@@ -0,0 +1,24 @@
+/// <reference types="cypress" />
+
+// Welcome to Cypress!
+//
+// This spec file contains a variety of sample tests
+// for a todo list app that are designed to demonstrate
+// the power of writing tests in Cypress.
+//
+// To learn more about how Cypress works and
+// what makes it such an awesome testing tool,
+// please read our getting started guide:
+// https://on.cypress.io/introduction-to-cypress
+
+describe('This is a simple activity', () => {
+  beforeEach(() => {
+    cy.visit('https://the-internet.herokuapp.com/')
+  })
+
+  it('The Home page has 44 Links', () => {
+    cy.get('a').should('have.length', 2)
+   
+  })
+})
+
