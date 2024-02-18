f1e7826cb5b1ce93fc489a1b1753d6f057fa0dd2
sureshnanjan <sureshnanjan@outlook.com>
2024-02-18 12:29:28 +0530

    Clarification for Cy.Intercept and a simple demo

--- cypress/e2e/RestfulBooker-API-Scenarios.cy.ts
+++ cypress/e2e/RestfulBooker-API-Scenarios.cy.ts
@@ -1,4 +1,4 @@
-describe("The Restful Booker CRUD Operations", {baseUrl:"https://restful-booker.herokuapp.com"},()=>{
+describe.skip("The Restful Booker CRUD Operations", {baseUrl:"https://restful-booker.herokuapp.com"},()=>{
     let token:string;
     let bookingdate:string
     it.skip("Health Check is OK", ()=>{
@@ -103,4 +103,49 @@ describe("The Restful Booker CRUD Operations", {baseUrl:"https://restful-booker.
     });
 
 
+});
+/**
+ * Cypress provides the cy.intercept() method, which allows you to spy on and stub network requests 
+ * made by your application. It operates at the network layer, meaning 
+ * it doesn’t actually make requests but listens to them as they occur.
+ * When you use cy.intercept(), Cypress sets up a network proxy in the browser.
+ * It catches requests made by your app and responds back to the browser.
+ * You can use it to simulate different server responses or network conditions during testing.
+ * You can intercept requests based on:
+ * URL (e.g., cy.intercept('/api/data'))
+ * HTTP method (e.g., cy.intercept('GET', '/api/data'))
+ * Custom route matchers (e.g., using an object with properties like method, url, hostname, etc.)
+ */
+describe("Intercepting Network Requests", ()=>{
+    /**
+     * The cy.request() command makes actual HTTP requests to the specified URL.
+     * It does not use the stubbed routes set up by cy.route() or cy.intercept().
+     * If you use cy.request(), it will hit the actual server, even if you’ve stubbed the same URL.
+     * These commands are specifically designed for stubbing network requests within your application.
+     * They intercept requests made by your app and provide predefined responses.
+     * They do not make actual HTTP requests to external servers.
+     */
+    it.only("Static Responses or Stubbing Demo", ()=>{
+        // Here after the stubbing the Heroku App home page return this
+        // stubbed response instead of the actual contents of the home page
+        cy.intercept('/*', { statusCode: 200, body: { message: 'Success' } }).as('demo');
+        cy.visit('/');
+        cy.wait('@demo').then((interception)=>{
+            
+            cy.log(JSON.stringify(interception.response?.body));
+            expect(interception?.response?.body).to.have.property("message","Success");
+    });
+        
+    });
+    it("Dynamic Handling using route handler functions Demo",()=>{
+        cy.intercept('/api/data', (req) => {
+            // Modify request or response as needed
+            req.reply({ statusCode: 200, body: { message: 'Custom response' } });
+          });
+          
+    });
+    it("Using route for simple stubbing",()=>{
+        
+
+    });
 });
